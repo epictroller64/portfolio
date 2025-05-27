84329c047c1347d3a8038496cfe9344889277404
epictroller64 <martin.ilbi@gmail.com>
2025-05-27 19:58:32 +0300

    Testing out SVG animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,79 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main>
+      <Uppersection></Uppersection>
+    </main>
   );
 }
+
+const Uppersection = () => (
+  <section className="bg-black w-full flex h-[50vh]">
+    <svg className="absolute w-full h-full">
+      <WhiteLine direction="left"></WhiteLine>
+      <WhiteLine direction="top"></WhiteLine>
+    </svg>
+    <div className="m-auto">
+      <p className="text-7xl text-center text-white">Welcome to my Portfolio</p>
+    </div>
+  </section>
+);
+
+function getLineCoordinates(direction: "left" | "right" | "top" | "bottom") {
+  switch (direction) {
+    case "left": {
+      return { x1: 0, y1: 10, x2: 100, y2: 10 };
+    }
+    case "top": {
+      return { x1: 10, y1: 10, x2: 10, y2: 110 };
+    }
+    case "bottom": {
+      return { x1: 10, y1: 10, x2: 10, y2: 110 };
+    }
+    case "right": {
+      return { x1: 0, y1: 10, x2: 100, y2: 10 };
+    }
+    default:
+      return { x1: 0, y1: 10, x2: 100, y2: 10 };
+  }
+}
+
+function renderRandomLines() {}
+
+const WhiteLine = ({
+  direction,
+}: {
+  direction: "left" | "right" | "top" | "bottom";
+}) => {
+  const ref = useRef<SVGLineElement>(null);
+  const [xy, setXy] = useState(getLineCoordinates(direction));
+  useEffect(() => {
+    const line = ref.current;
+    const incrementer = 0.5;
+    function animate() {
+      if (line) {
+        setXy((prev) => ({
+          ...prev,
+          x1: prev.x1 + incrementer,
+          x2: prev.x2 + incrementer,
+        }));
+      }
+      requestAnimationFrame(animate);
+    }
+    requestAnimationFrame(animate);
+  }, []);
+  return (
+    <line
+      ref={ref}
+      x1={xy.x1}
+      x2={xy.x2}
+      y1={xy.y1}
+      y2={xy.y2}
+      strokeWidth={2}
+      stroke="white"
+    ></line>
+  );
+};
